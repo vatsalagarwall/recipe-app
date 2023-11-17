c4ab086d7aae80e2f7dc04ad6bf1fcfb0ef75885
Vatsal Agarwal <bindalvatsaljpr@gmail.com>
2023-11-17 12:57:34 +0530

    commit

--- src/Components/Recipe.jsx
+++ src/Components/Recipe.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import './Recipe.css';
+import React from "react";
+import "./Recipe.css";
 
 const Recipe = ({ title, calories, image, ingredients, serves }) => {
   return (
     <div className="recipe">
       <h2>{title}</h2>
-      <p><b>Calories:</b> {Math.ceil(calories)}</p>
-      <p><b>Serves:</b> {serves}</p> 
+      <p>
+        <b>Calories:</b> {Math.ceil(calories)}
+      </p>
+      <p>
+        <b>Serves:</b> {serves}
+      </p>
       <img src={image} alt={title} />
-      <h3>Ingredients:</h3>
+      <h3>Main Ingredients:</h3>
       <ol>
         {ingredients.map((ingredient, index) => (
           <li key={index}>{ingredient.text}</li>
@@ -16,6 +20,6 @@ const Recipe = ({ title, calories, image, ingredients, serves }) => {
       </ol>
     </div>
   );
-}
+};
 
 export default Recipe;
