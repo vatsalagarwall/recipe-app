4636274d89104d26a9c79bb4ead36a4bf05fc1a3
Vatsal Agarwal <bindalvatsaljpr@gmail.com>
2023-11-21 15:03:05 +0530

    app.js

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
 
   const getRecipe = async () => {
     const response = await axios.get(`https://api.edamam.com/search?app_id=${API_ID}&app_key=${API_KEY}&q=${query}`)
+    console.log(response)
     setRecipes(response.data.hits)
     console.log(response.data.hits)
   }
